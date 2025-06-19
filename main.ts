7514aa570d197a1fb4701b61851980b51097b97c
Misha Farber Brodsky <misha@farberbrodsky.com>
2025-06-19 08:56:38 +0300

    cleanup

--- main.ts
+++ main.ts
@@ -115,23 +115,6 @@ export default class RPCPlugin extends Plugin {
         });
 
         /*
-        // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian RPC', (evt: MouseEvent) => {
-            // Called when the user clicks the icon.
-            new Notice('This is a notice!');
-        });
-        // Perform additional things with the ribbon
-        ribbonIconEl.addClass('my-plugin-ribbon-class');
-        */
-
-        // This adds a simple command that can be triggered anywhere
-        this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Open sample modal (simple)',
-            callback: () => {
-                new StatusModal(this.app).open();
-            }
-        });
         // This adds an editor command that can perform some operation on the current editor instance
         this.addCommand({
             id: 'sample-editor-command',
@@ -141,6 +124,17 @@ export default class RPCPlugin extends Plugin {
                 editor.replaceSelection('Sample Editor Command');
             }
         });
+        */
+        /*
+        // This creates an icon in the left ribbon.
+        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian RPC', (evt: MouseEvent) => {
+            // Called when the user clicks the icon.
+            new Notice('This is a notice!');
+        });
+        // Perform additional things with the ribbon
+        ribbonIconEl.addClass('my-plugin-ribbon-class');
+        */
+        /*
         // This adds a complex command that can check whether the current state of the app allows execution of the command
         this.addCommand({
             id: 'open-sample-modal-complex',
@@ -160,10 +154,12 @@ export default class RPCPlugin extends Plugin {
                 }
             }
         });
+        */
 
         // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new RPCSettingTab(this.app, this));
 
+        /*
         // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
         // Using this function will automatically remove the event listener when this plugin is disabled.
         this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
@@ -172,6 +168,7 @@ export default class RPCPlugin extends Plugin {
 
         // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
         this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+        */
     }
 
     onunload() {
